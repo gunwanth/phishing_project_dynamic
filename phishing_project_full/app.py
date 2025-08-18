c582af1b4c4b88c8d6e976ee3782c8c5396daf76
GUNVANTH MADABATTULA <145220883+gunwanth@users.noreply.github.com>
2025-08-18 23:21:44 +0530

    Update app.py

--- phishing_project_full/app.py
+++ phishing_project_full/app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import json
 import os
 from auth import login_block
-from streamlit_autorefresh import st_autorefresh
 from notifier import send_notification  # ✅ New import for phone alerts
 from phishing_project_full.gmail_client import GmailClient
 from phishing_project_full.phishing_detector import PhishingDetector
 from phishing_project_full.auth import login_block
+from streamlit_autorefresh import st_autorefresh
 from phishing_project_full.notifier import send_notification
 
 
